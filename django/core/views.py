031a1256699f786678e7f02ac1e75ec2cdb5c662
julia <donadellij@gmail.com>
2023-06-14 12:21:22 -0400

    Revisnado templates

--- django/core/views.py
+++ django/core/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
+from core.models import Produto
 
 # Create your views here.
 
 def index(request):
+    produtos = Produto.objects.all(
+        
+    )
     context = {
         'curso': "Programação web com django framework",
-        'outro': 'django e massa'
+        'outro': 'django e massa',
+        'produto' : 'produtos',
     }
     return render(request,'index.html', context)
 
 def contato(request):
     return render(request, 'contato.html')
+
+def produto(request, pk):
+    prod = Produto.objects.get(id=pk)
+    context = {
+        'produto' : prod
+    }
+    return render(request, 'produto.html', context)
