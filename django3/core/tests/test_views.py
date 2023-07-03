b3394a1deb91e6a855612eac6e4725da105cb5f0
Julia <donadellij@gmail.com>
2023-07-03 13:29:11 -0300

    Testes em django

---
+++ django3/core/tests/test_views.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from django.test import Client
+from django.urls import reverse_lazy
+
+class IndexViewTestCase(TestCase):
+    def setUp(self):
+         self.dados = {
+            'nome': "Julia",
+            'email': "julia@julia.com",
+            'assunto': "assunto",
+            'mensagem': "mensagem"
+        }
+         self.cliente = Client()
+
+    def test_form_valid(self):
+         request = self.cliente.post(reverse_lazy('index'), data=self.dados)
+         self.assertEquals(request.status_code, 302)
+    
+    def test_form_invalid(self):
+         self.dados = {
+            'nome': "Julia",
+            'email': "julia@julia.com",
+            'assunto': "assunto",
+        }
+         request = self.cliente.post(reverse_lazy('index'), data=self.dados)
+         self.assertEquals(request.status_code, 200)
+    
+        
